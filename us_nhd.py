3e44a11b1bdc441101430fad4fb0d356805d3812
Paul Norman <penorman@mac.com>
2012-04-30 00:50:30 -0700

    Complete most of the non-FCode translation code

--- us_nhd.py
+++ us_nhd.py
@@ -20,17 +20,25 @@ AreaSqKm        Area of feature             Duplicates geodata
 ComID           Unique ID of feature        Metadata
 FDate           Date of last modification   Metadata
 FType           Feature type                Duplication of FCode
+LengthKM        Feature length              Duplicates geodata
 OBJECTID        Object ID                   Metadata
 Permanent_      40 character GUID           Metadata
 Resolution      Resolution                  Metadata    
 Shape_Area      Area of feature             Duplicates geodata
 Shape_Leng      Length of feature           Duplicates geodata
+Source_Fea      Way that event is on        Duplicates geodata
+WBAreaComI          
+WBArea_Per
+Measure         Location along stream       Duplicates geodata
+Enabled
+
 
 The following fields are used:    
 
 Field           Used for            Reason
 Elevation       Elevation
 FCode           Mapping to OSM tags
+FeatureDet      Website of stream gage
 GNIS_Name       
 GNIS_ID         
 ReachCode       combining ways?
@@ -42,22 +50,54 @@ Source value                            OSM value                           Shor
 
 '''
 
+from osgeo import ogr
+
+def filterLayer(layer):
+    if not layer:
+        return
+    if layer.GetName() in ('NHDArea', 'NHDAreaEventFC', 'NHDFlowline', 'NHDLine', 
+                    'NHDLineEventFC', 'NHDPoint', 'NHDPointEventFC', 'NHDWaterbody'):
+        return layer
+    elif layer.GetName() in ('WBD_HU10', 'WBD_HU12', 'WBD_HU14', 'WBD_HU16', 'WBD_HU2', 'WBD_HU4', 'WBD_HU6', 'WBD_HU8', 'HYDRO_NET_Junctions'):
+        return
+    else:
+        print 'Unknown layer ' + layer.GetName()
+        return layer
+
 def filterTags(attrs):
     if not attrs:
         return
     tags = {}
     
-    '''
+
     for k,v in attrs.iteritems():
-        if k not in ('AreaSqKm', 'ComID', 'FDate', 'FType', 'OBJECTID', 'Permanent_', 'Resolution', 'Shape_Area', 'Shape_Leng', 'Elevation'):
-            tags['nhd:'+k]=v
+        if k not in ('AreaSqKm', 'ComID', 'FDate', 'FType', 'OBJECTID', 'Permanent_', 'Resolution', 
+        'Shape_Area', 'Shape_Leng', 'Elevation', 'LengthKM', 'GNIS_Name', 'WBAreaComI', 'WBArea_Per', 
+        'FeatureDet', 'Source_Fea', 'Enabled', 'FCode'):
+            if v != '':
+                tags['nhd:'+k]=v
             
     try:
         if 'Elevation' in attrs and float(attrs['Elevation'].strip()) != 0.:
-            tags['ele'] =  float(attrs['Elevation'].strip())
+            tags['ele'] =  str(float(attrs['Elevation'].strip()))
     except ValueError:
         pass
-    '''    
+
+    if 'GNIS_Name' in attrs and attrs['GNIS_Name'] != '':
+        tags['name'] = attrs['GNIS_Name']
+     
+    if 'FeatureDet' in attrs and attrs['FeatureDet'] != '':
+        tags['website'] = attrs['FeatureDet']
         
-    return attrs
+    if 'FCode' in attrs and attrs['FCode'] != '':
+        tags['gnis:fcode'] = attrs['FCode']
+        if attrs['FCode'] == '55800':
+            if 'name' in tags and 'river'.upper() in tags['name'].upper():
+                tags['waterway'] = 'river'
+            else:
+                tags['waterway'] = 'stream'
+        else:
+            tags['fixme'] = 'Unknown FCode ' + attrs['FCode']
+            
+    return tags
     
