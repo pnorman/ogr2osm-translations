0be705930cfe05d7dc501a269d5365c908b511de
Paul Norman <penorman@mac.com>
2012-08-18 11:07:57 -0700

    Partial hydrant translation

--- SurreyCombined.py
+++ SurreyCombined.py
@@ -59,8 +59,7 @@ def filterFeature(ogrfeature, fieldNames, reproject):
 
 
 def filterLayer(layer):
-    if not layer:
-        return
+    if not layer: return
     
     layername = layer.GetName()
     
@@ -78,3 +77,61 @@ def filterLayer(layer):
     
     layer.ResetReading()
     return layer
+
+'''
+Map Surrey -> OSM
+'''
+def filterTags(attrs):
+    if not attrs: return
+    tags = {}
+
+    ''' Delete common useless tags '''
+    if 'YTD_COST' in attrs:
+            del attrs['YTD_COST']
+    if '__LAYER' in attrs and attrs['__LAYER'] == 'wtrHydrantsSHP':
+        if 'BV_MANUFAC' in attrs:
+            del attrs['BV_MANUFAC']
+        if 'BV_MODEL' in attrs:
+            del attrs['BV_MODEL']
+        if 'COMMENTS' in attrs:
+            del attrs['COMMENTS']
+        if 'DIS2VALVE' in attrs: # Distance to valve
+            del attrs['DIS2VALVE']
+        if 'PROJECT_NO' in attrs:
+            del attrs['PROJECT_NO']
+        if 'WARR_DATE' in attrs:
+            del attrs['WARR_DATE']
+
+        tags['emergency'] = 'fire_hydrant'
+
+        if 'HYD_TYPE' in attrs:
+            if attrs['HYD_TYPE'].strip() == '0':
+                tags['fire_hydrant:type'] = 'pillar'
+            elif attrs['HYD_TYPE'].strip() == '1':
+                tags['fire_hydrant:type'] = 'underground'
+            else:
+                l.error("Unknown HYD_TYPE=%s in %s" % (attrs['HYD_TYPE'].strip(), attrs['__LAYER']))
+                tags['fixme'] = 'yes'
+                if 'HYD_TYPE2' in attrs:
+                    tags['surrey:HYD_TYPE'] = attrs['HYD_TYPE2'].strip()
+                else:
+                    tags['surrey:HYD_TYPE'] = attrs['HYD_TYPE'].strip()
+            del attrs['HYD_TYPE']
+            if 'HYD_TYPE2' in attrs:
+                del attrs['HYD_TYPE2']
+
+        if 'MAKE' in attrs:
+            tags['manufacturer'] = attrs['MAKE']
+            del attrs['MAKE']
+                
+        if 'OWNER' in attrs:
+            tags['owner'] = attrs['OWNER']
+            del attrs['OWNER']
+        
+        if 'YR' in attrs:
+            tags['start_date'] = attrs['YR']
+            del attrs['YR']
+        
+    tags.update(attrs)
+    
+    return tags
